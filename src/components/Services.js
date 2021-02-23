756a91ed9554c54d5116576846f7c76b321d8d09
Kristen <kristenwood123@gmail.com>
2021-02-23 12:32:12 -0800

    styled services

--- src/components/Services.js
+++ src/components/Services.js
@@ -4,49 +4,62 @@ import {FaHome, FaDollarSign, FaChartLine, FaRegBuilding, FaSearch, FaMobile } f
 function Services() {
   return (
     <section className='services'>
+      <h2>Services</h2>
       <div className="services__container">
-        <div>
-          <div><FaHome/></div>
-          <h3>Search Property</h3>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis quis molestiae vitae eligendi at.</p>
-          <a href="#">Learn More</a>
-        </div>
+        <div className='services__item'>
+           <span className='services__icon'><FaHome/></span>
+              <div className="services__item__container">
+                <h3>Search Property</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis quis molestiae vitae eligendi at.</p>
+                <a href="#">Learn More</a>
+              </div>
+          </div>
         
-        <div>
-          <div><FaDollarSign/></div>
-          <h3>Buy Property</h3>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis quis molestiae vitae eligendi at.</p>
-          <a href="#">Learn More</a>
+        <div className='services__item'>
+          <span className='services__icon'><FaDollarSign/></span>
+            <div className="services__item__container">
+              <h3>Buy Property</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis quis molestiae vitae eligendi at.</p>
+              <a href="#">Learn More</a>
+            </div>
         </div>
 
-        <div>
-          <div><FaChartLine/></div>
-          <h3>Search Property</h3>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis quis molestiae vitae eligendi at.</p>
-          <a href="#">Learn More</a>
+        <div className='services__item'>
+          <span className='services__icon'><FaChartLine/></span>
+            <div className="services__item__container">
+              <h3>Search Property</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis quis molestiae vitae eligendi at.</p>
+              <a href="#">Learn More</a>
+          </div>
         </div>
 
-        <div>
-          <div><FaRegBuilding/></div>
-          <h3>Investing</h3>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis quis molestiae vitae eligendi at.</p>
-          <a href="#">Learn More</a>
+        <div className='services__item'>
+          <span className='services__icon'><FaRegBuilding/></span>
+            <div className="services__item__container">
+              <h3>Investing</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis quis molestiae vitae eligendi at.</p>
+              <a href="#">Learn More</a>
+          </div>
         </div>
 
-        <div>
-          <div><FaSearch/></div>
-          <h3>List a Property</h3>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis quis molestiae vitae eligendi at.</p>
-          <a href="#">Learn More</a>
-        </div>
-
-        <div>
-          <div><FaMobile/></div>
-          <h3>Property Location</h3>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis quis molestiae vitae eligendi at.</p>
-          <a href="#">Learn More</a>
+        <div className='services__item'>
+          <span className='services__icon'><FaSearch/></span>
+            <div className="services__item__container">
+              <h3>List a Property</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis quis molestiae vitae eligendi at.</p>
+              <a href="#">Learn More</a>
         </div>
       </div>
+
+        <div className='services__item'>
+          <span className='services__icon'><FaMobile/></span>
+            <div className="services__item__container">
+              <h3>Property Location</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis quis molestiae vitae eligendi at.</p>
+              <a href="#">Learn More</a>
+        </div>
+      </div>
+    </div>
     </section>
   )
 }
